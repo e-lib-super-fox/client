3c28dd21e1bf74e953ab7dd6d2476f85c6549989
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-06-08 00:37:10 +0700

    testing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,29 @@
 <template>
     <div>
+<<<<<<< HEAD
         <GetAllBook v-if="LoggedIn"/>
         <Login v-else/>
+=======
+        <GetAllBook/>
+>>>>>>> some checking on login
     </div>
 </template>
 <script>
 import GetAllBook from './GetAllBook.vue'
+<<<<<<< HEAD
 import Login from './Login.vue'
 export default{
     components:{ GetAllBook, Login },
     data () {
         return {
             LoggedIn:false
+=======
+export default{
+    components:{ GetAllBook },
+    data () {
+        return {
+
+>>>>>>> some checking on login
         }
     }
 }
