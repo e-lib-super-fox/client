65f14a072582cdc288e8fb50003d498e739bf61c
annisaayu <annisaayu569@gmail.com>
2018-06-07 20:28:28 +0700

    fitur login

---
+++ src/components/DetailBook.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col cols="3">col-8</b-col>
+      <b-col cols="7">col-4</b-col>
+      <b-col cols="2">
+        <b-button variant="primary"> Download </b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'login',
+  data () {
+    return {
+    }
+  },
+  methods : {
+    
+  }
+}
+</script>
+
+<style scoped>
+</style>
