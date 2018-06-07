f9b58c4e69fa45c3767cd2bafd79bb6caf5c36e2
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-06-08 00:18:04 +0700

    some checking on login

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <GetAllBook/>
+    </div>
+</template>
+<script>
+import GetAllBook from './GetAllBook.vue'
+export default{
+    components:{ GetAllBook },
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+<style>
+
+
+</style>
