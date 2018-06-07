06e5ee7372ad1acda7df6e88fafaeb300e0661c7
annisaayu <annisaayu569@gmail.com>
2018-06-07 21:29:43 +0700

    fitur detail buku

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import DetailBook from './views/DetailBook.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/detail-book/',
+      name: 'detailBook',
+      component: DetailBook
     },
     {
       path: '/about',
