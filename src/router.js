28ad15c340abdb0b99e32faa957ff4ac01fd9020
restuu <36615830+restuu@users.noreply.github.com>
2018-06-08 14:52:55 +0700

    conflict

--- src/router.js
+++ src/router.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-<<<<<<< HEAD
 import DetailBook from './views/DetailBook.vue'
-=======
->>>>>>> 6da2749f598bbb9b979aeb0e9101da34f0df6c07
 import AddBook from './views/AddBook.vue'
 
 Vue.use(Router)
@@ -18,16 +15,12 @@ export default new Router({
       component: Home
     },
     {
-<<<<<<< HEAD
       path: '/detail-book/:isbn',
       name: 'detailBook',
       component: DetailBook
     },
     {
       path: '/add-book',
-=======
-      path: '/books/add',
->>>>>>> 6da2749f598bbb9b979aeb0e9101da34f0df6c07
       name: 'addbook',
       component: AddBook
     }
